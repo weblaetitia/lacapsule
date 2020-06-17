642f13f26b6787c96784008d6f9416096244fff3
Laetitia Langlois <weblaetitia@hotmail.com>
2020-06-17 16:17:00 +0200

    Add Prioritize chalenge (to improve)

---
+++ Semaine 1/Jour 3/prioritize/script.js
@@ -0,0 +1,36 @@
+// 👉 Via le DOM, en Javascript,  vous allez devoir modifier la couleur des tâches en respectant la maquette: 
+// 1 : vert
+// 2 : jaune
+// 3 : orange
+// 4 : rouge
+
+var priorityArray = document.getElementsByClassName('criticality');
+console.log(priorityArray);
+
+for (var i=0; i<priorityArray.length; i++) {
+    var number = priorityArray[i].textContent;
+    if (number == 1) {
+        priorityArray[i].classList.add('green');
+    } else if (number == 2) {
+        priorityArray[i].classList.add('yellow');
+    } else if (number == 3) {
+        priorityArray[i].classList.add('orange');
+    } else if (number == 4) {
+        priorityArray[i].classList.add('red');
+    }
+}
+
+// 👉 Mettre en place une mécanique permettant, au chargement, d’afficher uniquement les tâches de priorité 3 & 4.
+
+var emergencyBtn = document.getElementById('emergency-btn');
+
+emergencyBtn.addEventListener('click', function() {
+    for (var i=0; i<priorityArray.length; i++) {
+        var number = priorityArray[i].textContent;
+        console.log(number)
+        if ((number == 1) || (number == 2)) {
+            priorityArray[i].parentNode.remove();
+            console.log('removed')
+        } 
+    }
+})
